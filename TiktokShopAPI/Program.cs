8b67197bdd0414cbbc7eac2fdab5b767f51f14c5
TorikatoTrong <101616337+TorikatoTrong@users.noreply.github.com>
2024-07-11 10:20:37 +0700

    Add project files.

---
+++ TiktokShopAPI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TiktokShopAPI.Module.TikTokShopApi;
+
+namespace TiktokShopAPI;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        #region Private Data
+
+        var appKey = "";
+        var appSecret = "";
+        var accessToken = "";
+        var shopCipher = "";// Use Function GetAuthorizedShops To Get This
+
+        #endregion
+        
+        var tiktokShopAuthorized = new TiktokShopAuthorized(appKey, appSecret, accessToken, shopCipher);
+        var tiktokShopApi = new TiktokShopApi(tiktokShopAuthorized);
+
+        //var shop = await tiktokShopApi.GetAuthorizedShops();
+        //Console.WriteLine(shop);
+
+        var jsonString = await tiktokShopApi.GetAuthorizedShops();
+        SaveJsonToFile(JObject.Parse(jsonString), "AuthorizedShops");
+
+
+        Console.ReadKey();
+    }
+    public static void SaveJsonToFile(JObject jsonData, string filename = "TiktokData")
+    {
+        var json = JsonConvert.SerializeObject(jsonData, Formatting.Indented);
+        var fullPath = Path.Combine(@"D:\TiktokData\", $"{filename}.json");
+        File.WriteAllText(fullPath, json);
+        Console.WriteLine($"Data has been saved to {fullPath}");
+    }
+}
---
+++ TiktokShopAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TiktokAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TiktokAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f7cf317-4435-4a8e-a9e3-3a8a13df0a34")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
